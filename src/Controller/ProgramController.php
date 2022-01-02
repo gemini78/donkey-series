4c7f85b9ecb44b2702f2a41721f96abd506e1c61
Stebou <stebou8mendy@gmail.com>
2022-01-02 09:04:57 +0100

    Added new route with parameter and method

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,14 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'program')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ProgramController.php',
+        ]);
+    }
+
+    #[Route('/{id}', methods: ['get'], requirements: ['id' => '\d+'], name: 'show')]
+    public function show(int $id): Response
+    {
+
+        return $this->render('program/show.html.twig', [
+            'id' => $id
         ]);
     }
 }
